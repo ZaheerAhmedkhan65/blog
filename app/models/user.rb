6739e045557c33da072a906d946df9c9a03ed3ac
ZaheerAhmedkhan65 <ranazaheerahmed65@gmail.com>
2024-11-17 04:35:24 +0500

    fixed spacing issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :registerable, :timeoutable, :trackable and :omniauthable 
+  # :confirmable, :lockable, :registerable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :recoverable, :rememberable, :validatable
   validates :username, presence: true, uniqueness: true
-  
 end
